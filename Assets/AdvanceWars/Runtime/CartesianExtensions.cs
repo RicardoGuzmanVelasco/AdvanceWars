132ba1d988267bd5d370df9e8340a8093b7ed0a4
RGV <ricardo.guzman.velasco@outlook.com>
2022-08-14 19:24:24 +0200

    refactor: extract cartesian extensions file

---
+++ Assets/AdvanceWars/Runtime/CartesianExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace AdvanceWars.Runtime
+{
+    public static class CartesianExtensions
+    {
+        public static IEnumerable<Vector2Int> CoordsAdjacentsOf(this Vector2Int from)
+        {
+            return new[]
+            {
+                from + Vector2Int.up,
+                from + Vector2Int.down,
+                from + Vector2Int.left,
+                from + Vector2Int.right
+            };
+        }
+    }
+}
--- Assets/AdvanceWars/Runtime/Map.cs
+++ Assets/AdvanceWars/Runtime/Map.cs
@@ -48,18 +48,4 @@ namespace AdvanceWars.Runtime
             occupiedCoords_MOCK.Add(coord);
         }
     }
-
-    public static class CartesianExtensions
-    {
-        public static IEnumerable<Vector2Int> CoordsAdjacentsOf(this Vector2Int from)
-        {
-            return new[]
-            {
-                from + Vector2Int.up,
-                from + Vector2Int.down,
-                from + Vector2Int.left,
-                from + Vector2Int.right
-            };
-        }
-    }
 }
