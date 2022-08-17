96bd8e46562764dee52a350d214d511597d98be8
RGV <ricardo.guzman.velasco@outlook.com>
2022-08-17 05:59:01 +0200

    test: add unit builder

--- Assets/AdvanceWars/Tests/FireTests.cs
+++ Assets/AdvanceWars/Tests/FireTests.cs
@@ -3,6 +3,7 @@ using FluentAssertions;
 using NUnit.Framework;
 using static AdvanceWars.Tests.BatallionBuilder;
 using static AdvanceWars.Tests.TerrainBuilder;
+using static AdvanceWars.Tests.UnitBuilder;
 using static AdvanceWars.Tests.WeaponBuilder;
 using Batallion = AdvanceWars.Runtime.Batallion;
 
@@ -14,16 +15,16 @@ namespace AdvanceWars.Tests
         public void Weapon_DoesNoDamage_ByDefault()
         {
             Weapon().Build()
-                .BaseDamageTo(new Unit())
+                .BaseDamageTo(Unit().Build())
                 .Should().Be(0);
         }
 
         [Test]
         public void AssignedBaseDamage_IsGreaterThanZero()
         {
-            var sut = Weapon().WithDamage(new Unit(), 10).Build();
-
-            sut.BaseDamageTo(new Unit())
+            var theSameUnit = Unit().Build();
+            Weapon().WithDamage(theSameUnit, 10).Build()
+                .BaseDamageTo(theSameUnit)
                 .Should().Be(10);
         }
 
@@ -32,8 +33,9 @@ namespace AdvanceWars.Tests
         [TestCase(9, 1)]
         public void Platoons_Derives_FromForces(int forces, int effectivity)
         {
-            var sut = Batallion().WithForces(forces).Build();
-            sut.Platoons.Should().Be(effectivity);
+            Batallion().WithForces(forces).Build()
+                .Platoons
+                .Should().Be(effectivity);
         }
 
         [TestCase(100, 1)]
@@ -41,55 +43,58 @@ namespace AdvanceWars.Tests
         [TestCase(9, .1f)]
         public void OffensiveEffectivity_Derives_FromForces(int offForces, float expected)
         {
-            var sut = new Offensive
-            (
-                attacker: Batallion().WithForces(offForces).Build(),
-                defender: Batallion.Null
-            );
-            sut.Effectivity.Should().Be(expected);
+            new Offensive
+                (
+                    attacker: Batallion().WithForces(offForces).Build(),
+                    defender: Batallion.Null
+                )
+                .Effectivity
+                .Should().Be(expected);
         }
 
         [Test]
         public void DamageReduction_WhenTerrain_DoesntCover()
         {
-            var sut = new Offensive
-            (
-                attacker: Batallion.Null,
-                defender: Batallion().WithForces(100).Build()
-            );
-            sut.DamageReductionMultiplier.Should().Be(1);
+            new Offensive
+                (
+                    attacker: Batallion.Null,
+                    defender: Batallion().WithForces(100).Build()
+                )
+                .DamageReductionMultiplier
+                .Should().Be(1);
         }
 
         [TestCase(1, 100, 0.9f)]
         [TestCase(1, 50, 0.95f)]
         public void DamageReduction_WhenTerrain_Cover(int defensiveRating, int forces, float expected)
         {
-            var sut = new Offensive
-            (
-                attacker: Batallion.Null,
-                defender: Batallion().WithForces(forces).Build(),
-                battlefield: Terrain().WithDefense(defensiveRating).Build()
-            );
-            sut.DamageReductionMultiplier.Should().Be(expected);
+            new Offensive
+                (
+                    attacker: Batallion.Null,
+                    defender: Batallion().WithForces(forces).Build(),
+                    battlefield: Terrain().WithDefense(defensiveRating).Build()
+                )
+                .DamageReductionMultiplier
+                .Should().Be(expected);
         }
 
         [Test]
         public void BaseDamage_ObtainedFromWeapon()
         {
+            var theSameUnit = Unit().Build();
             var sut = Batallion()
                 .WithWeapon
-                    (Weapon().WithDamage(new Unit(), 10).Build())
+                    (Weapon().WithDamage(theSameUnit, 10).Build())
                 .Build();
 
-            sut.BaseDamageTo(new Unit())
+            sut.BaseDamageTo(theSameUnit)
                 .Should().Be(10);
         }
 
         [Test]
-        public void METHOD()
+        public void FinalDamage_FromFormula()
         {
             var weapon = Weapon().WithDamage(Batallion().Build().Unit, 100).Build();
-            var unit = UnitBuilder.Unit()...;
             var sut = new Offensive(
                 attacker: Batallion().WithWeapon(weapon).WithForces(100).Build(),
                 defender: Batallion().WithForces(50).Build(),
---
+++ Assets/AdvanceWars/Tests/UnitBuilder.cs
@@ -0,0 +1,48 @@
+﻿using AdvanceWars.Runtime;
+
+namespace AdvanceWars.Tests
+{
+    internal class UnitBuilder
+    {
+        MovementRate mobility = 0;
+        Propulsion propulsion = new Propulsion("");
+        Weapon weapon = WeaponBuilder.Weapon().Build();
+
+        #region ObjectMothers
+        public static UnitBuilder Unit()
+        {
+            return new UnitBuilder();
+        }
+        #endregion
+
+        #region FluentAPI
+        public UnitBuilder WithMobility(MovementRate mobility)
+        {
+            this.mobility = mobility;
+            return this;
+        }
+
+        public UnitBuilder With(Propulsion propulsion)
+        {
+            this.propulsion = propulsion;
+            return this;
+        }
+
+        public UnitBuilder With(Weapon weapon)
+        {
+            this.weapon = weapon;
+            return this;
+        }
+        #endregion
+
+        public Unit Build()
+        {
+            return new Unit
+            {
+                Mobility = mobility,
+                Propulsion = propulsion,
+                Weapon = weapon
+            };
+        }
+    }
+}
