647b1fea74948dec2e5f49c2e26db67262f409d6
RGV <ricardo.guzman.velasco@outlook.com>
2022-08-17 21:59:54 +0200

    test: extract test fixtures by cohesive testing

---
+++ Assets/AdvanceWars/Tests/RangeOfMovementTests.cs
@@ -0,0 +1,117 @@
+﻿using AdvanceWars.Runtime;
+using FluentAssertions;
+using NUnit.Framework;
+using UnityEngine;
+using static AdvanceWars.Tests.Builders.BatallionBuilder;
+
+namespace AdvanceWars.Tests
+{
+    public class RangeOfMovementTests
+    {
+        [Test]
+        public void Map_WithSizeOne_HasNoRangeOfMovement()
+        {
+            new Map(1, 1)
+                .RangeOfMovement(from: Vector2Int.zero, rate: 1)
+                .Should().BeEmpty();
+        }
+
+        [Test]
+        public void IfNoMovementRate_ThenNoRangeOfMovement()
+        {
+            new Map(1, 2)
+                .RangeOfMovement(from: Vector2Int.zero, rate: 0)
+                .Should().BeEmpty();
+        }
+
+        [Test]
+        public void IfThereIsSpaceAndMovementRate_ThenThereIsRangeOfMovement()
+        {
+            new Map(1, 2)
+                .RangeOfMovement(from: Vector2Int.zero, rate: 1)
+                .Should().NotBeEmpty();
+        }
+
+        [Test]
+        public void WithMovementRate1_RangeOfMovementDoesNotIncludeDiagonals()
+        {
+            new Map(2, 2)
+                .RangeOfMovement(from: Vector2Int.zero, rate: 1)
+                .Should().NotContain(Vector2Int.one);
+        }
+
+        [Test]
+        public void WithMovementRateGreaterThan1_RangeOfMovementIncludesDiagonals()
+        {
+            new Map(2, 2)
+                .RangeOfMovement(from: Vector2Int.zero, rate: 2)
+                .Should().Contain(Vector2Int.one);
+        }
+
+        [Test]
+        public void IfThereIsSpaceAndMovementRate_ThenRangeOfMovementIncludesAdjacents()
+        {
+            new Map(2, 2)
+                .RangeOfMovement(from: Vector2Int.zero, rate: 1)
+                .Should()
+                .Contain(Vector2Int.up).And.Contain(Vector2Int.right);
+        }
+
+        [Test]
+        public void RangeOfMovement_NeverIncludesOriginalPosition()
+        {
+            new Map(2, 2)
+                .RangeOfMovement(from: Vector2Int.zero, rate: 1)
+                .Should().NotContain(Vector2Int.zero);
+
+            new Map(3, 3)
+                .RangeOfMovement(from: Vector2Int.one, rate: 4)
+                .Should().NotContain(Vector2Int.one);
+        }
+
+        [Test]
+        public void RangeOfMovement_NeverIncludes_anOccupiedPosition()
+        {
+            var occupiedPos = Vector2Int.up;
+            var sut = new Map(2, 2);
+            sut.Put(occupiedPos, Batallion().Build());
+
+            sut.RangeOfMovement(from: Vector2Int.zero, rate: 1)
+                .Should().NotContain(occupiedPos);
+        }
+
+        [Test]
+        public void RangeOfMovement_isAlsoComputable_FromTroops()
+        {
+            var troop = Batallion().Build();
+            var sut = new Map(3, 3);
+            sut.Put(Vector2Int.one, troop);
+
+            sut.RangeOfMovement(troop)
+                .Should().NotBeEmpty();
+        }
+
+        [Test]
+        public void EnemyBlocksTheWay_SoRangeOfMovementStopsOnEnemy()
+        {
+            var aTroop = Infantry().Friend().Build();
+            var sut = new Map(1, 3);
+            sut.Put(Vector2Int.zero, aTroop);
+            sut.Put(Vector2Int.up, Infantry().Enemy().Build());
+
+            sut.RangeOfMovement(aTroop)
+                .Should().BeEmpty();
+        }
+
+        [Test]
+        public void MovementRate_Limits_RangeOfMovement()
+        {
+            var troop = Batallion().WithMoveRate(2).Build();
+            var sut = new Map(1, 6);
+            sut.Put(Vector2Int.zero, troop);
+
+            sut.RangeOfMovement(troop)
+                .Should().HaveCount(2);
+        }
+    }
+}
---
+++ Assets/AdvanceWars/Tests/TroopsThroughTerrainsTests.cs
@@ -0,0 +1,56 @@
+﻿using AdvanceWars.Runtime;
+using AdvanceWars.Tests.Builders;
+using FluentAssertions;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace AdvanceWars.Tests
+{
+    public class TroopsThroughTerrainsTests
+    {
+        [Test]
+        public void SpaceIsHostile_WhetherOccupiedBy_anEnemy()
+        {
+            var friend = BatallionBuilder.Infantry().Friend().Build();
+            var sut = new Map.Space { Occupant = BatallionBuilder.Infantry().Enemy().Build() };
+
+            sut.IsHostileTo(friend)
+                .Should().BeTrue();
+        }
+
+        [Test]
+        public void NonDefinedPropulsions_CostOne()
+        {
+            var sut = TerrainBuilder.Terrain().WithCost(new Propulsion("NotA"), 2).Build();
+            sut.MoveCostOf(new Propulsion("A")).Should().Be(1);
+        }
+
+        [Test]
+        public void Unit_CannotCross_BlockerTerrain()
+        {
+            //Arrange
+            var blockedProp = new Propulsion("Whatever");
+            var unit = BatallionBuilder.Infantry().WithPropulsion(blockedProp).Build();
+            var sut = new Map(1, 3);
+
+            sut.Put(Vector2Int.zero, unit);
+            sut.Put(Vector2Int.up, TerrainBuilder.Terrain().WithBlocked(blockedProp).Build());
+
+            //Act
+            var result = sut.RangeOfMovement(unit);
+
+            //Assert
+            result.Should().BeEmpty();
+        }
+
+
+        //no exponer unit en batallion
+        //eliminar el problema de tener que estar comprobando si existe el espacio.
+        //O bien iniciar los espacios o bien estructura para que se inicien lazy al acceder a ellos.
+        //el caso de blocker ahora mismo está mockeado.
+        //public space at
+        //no deberia de haber set en el terreno
+        //Movement Cost
+        //duplicacion espacios y bounds. Nullcheck espacio en range of movement
+    }
+}
