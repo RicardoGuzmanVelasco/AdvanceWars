63bc61c4b493f02dfa5ba7b67fbef079a6029b2c
Xiphereal <lozanojalex@gmail.com>
2022-09-08 19:06:42 +0200

    refactor: add basic implementation for building a Map from a formatted string

---
+++ Assets/AdvanceWars/Tests/Builders/MapBuilder.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+using AdvanceWars.Runtime;
+
+namespace AdvanceWars.Tests.Builders
+{
+    public class MapBuilder
+    {
+        int sizeX;
+        int sizeY;
+
+        #region ObjectMothers
+        public static MapBuilder Map() => new MapBuilder();
+        #endregion
+
+        public MapBuilder Of(int sizeX, int sizeY)
+        {
+            this.sizeX = sizeX;
+            this.sizeY = sizeY;
+
+            return this;
+        }
+        
+        public MapBuilder WithStructure(string mapAsVerbatimString)
+        {
+            var lines = mapAsVerbatimString.Split("\n");
+            
+            this.sizeX = SpacesIn(lines.First());
+            this.sizeY = lines.Length;
+
+            return this;
+        }
+
+        int SpacesIn(string row)
+        {
+            return row.Count(x => x == 'O');
+        }
+        
+        public Map Build()
+        {
+            return new Map(this.sizeX, this.sizeY);
+        }
+    }
+}
