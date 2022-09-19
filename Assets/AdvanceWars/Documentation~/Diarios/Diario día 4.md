8357b6c7e68e69c80941b7f8b923e536538f08a1
Portatil Angel <culoextremo@gmail.com>
2022-09-19 12:06:44 +0200

    test: add first battalion merge test
    this test worked unexpectedly, more functionality regarding the
    availability of move tactic is needed

--- Assets/AdvanceWars/Documentation~/Diarios/Diario día 4.md
+++ Assets/AdvanceWars/Documentation~/Diarios/Diario día 4.md
@@ -7,7 +7,8 @@ Duración: 3H
 ### Intenciones
 
 - [ ] Disponibilidad de tactica de mergear unidades
+    - [ ] Una unidad no debe de poder moverse si no tiene huecos
 - [ ] Mergear unidades si da tiempo
 
 ### Conclusiones
-
+- El test de que una unidad dañada puede moverse a otra unidad de su mismo tipo, funcionó inesperadamente. Faltaba funcionalidad referente a Tactic.Move
---
+++ Assets/AdvanceWars/Tests/BattalionMergeTests.cs
@@ -0,0 +1,26 @@
+﻿using AdvanceWars.Runtime;
+using FluentAssertions;
+using NUnit.Framework;
+using UnityEngine;
+using static AdvanceWars.Tests.Builders.BattalionBuilder;
+using static AdvanceWars.Tests.Builders.CommandingOfficerBuilder;
+
+namespace AdvanceWars.Tests
+{
+    public class BattalionMergeTests
+    {
+        [Test]
+        public void MoveTactic_AvailableTo_JoinableBattalions()
+        {
+            var anAllyBattallion = Battalion().WithNation("sameNation").WithForces(100).Build();
+            var anotherAllyBattallion = Battalion().WithNation("sameNation").WithForces(10).Build();
+            var map = new Map(1, 2);
+            map.Put(Vector2Int.zero, anAllyBattallion);
+            map.Put(Vector2Int.up, anotherAllyBattallion);
+            var sut = CommandingOfficer().WithNation("sameNation").WithMap(map).Build();
+
+            sut.AvailableTacticsOf(anAllyBattallion)
+                .Should().Contain(Tactic.Move);
+        }
+    }
+}
