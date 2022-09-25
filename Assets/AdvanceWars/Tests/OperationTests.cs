8b51e2eec463d61721ca62e43a95f7d31f903996
Xiphereal <lozanojalex@gmail.com>
2022-09-25 18:29:25 +0200

    test: add test to ensure an Space without a building does not heal a Battalion
    Principalmente subo test sin lanzar porque cambio de PC, y en el portatil Unity no para de crashear cada X lanzamientos.

--- Assets/AdvanceWars/Tests/OperationTests.cs
+++ Assets/AdvanceWars/Tests/OperationTests.cs
@@ -7,6 +7,7 @@ using UnityEngine;
 using static AdvanceWars.Tests.Builders.BattalionBuilder;
 using static AdvanceWars.Tests.Builders.CommandingOfficerBuilder;
 using static AdvanceWars.Tests.Builders.MapBuilder;
+using static AdvanceWars.Tests.Builders.TerrainBuilder;
 
 namespace AdvanceWars.Tests
 {
@@ -71,7 +72,7 @@ namespace AdvanceWars.Tests
             var sut = new Building(siegePoints: 20);
             map.Put(new Vector2Int(0, 0), sut);
 
-            const string aNation = "aNation";
+            var aNation = "aNation";
             var battalion = Battalion().WithNation(aNation).WithForces(10).Build();
             map.Put(new Vector2Int(0, 0), battalion);
 
@@ -95,7 +96,8 @@ namespace AdvanceWars.Tests
             var sut = new Building(siegePoints: 20);
             map.Put(new Vector2Int(0, 0), sut);
 
-            var battalion = Battalion().WithNation("aNation").WithForces(1).Build();
+            var initialForces = 1;
+            var battalion = Battalion().WithNation("aNation").WithForces(initialForces).Build();
             map.Put(new Vector2Int(0, 0), battalion);
 
             var commandingOfficers = new[]
@@ -107,7 +109,7 @@ namespace AdvanceWars.Tests
             
             operation.EndTurn();
 
-            battalion.Forces.Should().Be(1);
+            battalion.Forces.Should().Be(initialForces);
         }
         
         [Test]
@@ -118,7 +120,7 @@ namespace AdvanceWars.Tests
             var sut = new Building(siegePoints: 20);
             map.Put(new Vector2Int(0, 0), sut);
             
-            const string aNation = "aNation";
+            var aNation = "aNation";
             var battalion = Battalion().WithNation(aNation).Build();
             map.Put(new Vector2Int(0, 0), battalion);
 
@@ -132,6 +134,31 @@ namespace AdvanceWars.Tests
             operation.EndTurn();
 
             battalion.Forces.Should().Be(100);
+        }
+        
+        [Test]
+        public void NonBuildingInSpace_DoesNotHealBattalion_OnNewTurnBeginning()
+        {
+            var map = Map().Of(1,1).Build();
+
+            var nonBuildingTerrain = Terrain().Build();
+            map.Put(new Vector2Int(0, 0), nonBuildingTerrain);
+            
+            var initialForces = 50;
+            var aNation = "aNation";
+            var battalion = Battalion().WithForces(initialForces).WithNation(aNation).Build();
+            map.Put(new Vector2Int(0, 0), battalion);
+
+            var commandingOfficers = new[]
+            {
+                CommandingOfficer().Build(),
+                CommandingOfficer().WithMap(map).WithNation(aNation).Build()
+            };
+            var operation = new Operation(commandingOfficers, map);
+            
+            operation.EndTurn();
+
+            battalion.Forces.Should().Be(initialForces);
         }
     }
 }
