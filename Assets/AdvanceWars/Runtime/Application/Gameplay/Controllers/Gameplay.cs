241c3bc1fdce5e4fe26f9eeb80b71a85207f8813
Ángel Siendones SIllero <culoextremo@gmail.com>
2022-12-23 19:57:40 +0100

    refactor: draw map from super controller

--- Assets/AdvanceWars/Runtime/Application/Gameplay/Controllers/Gameplay.cs
+++ Assets/AdvanceWars/Runtime/Application/Gameplay/Controllers/Gameplay.cs
@@ -8,14 +8,17 @@ namespace AdvanceWars.Runtime.Application
     public class Gameplay
     {
         private readonly PlayTurn playTurn;
+        private readonly DrawMap drawMap;
 
-        public Gameplay(PlayTurn playTurn)
+        public Gameplay(PlayTurn playTurn, DrawMap drawMap)
         {
             this.playTurn = playTurn;
+            this.drawMap = drawMap;
         }
 
         public async Task Run()
         {
+            await drawMap.Run();
             while (true)
             {
                 await playTurn.Run();
