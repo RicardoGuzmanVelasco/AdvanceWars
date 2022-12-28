9b6dedfe1b590cef4634faf8b568bf414c169544
Xiphereal <lozanojalex@gmail.com>
2022-12-28 22:36:34 +0100

    refactor: add a ceiled int as a utility

---
+++ Assets/AdvanceWars/Runtime/Extensions/DataStructures/CeiledInt.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace AdvanceWars.Runtime.Extensions.DataStructures
+{
+    public class CeiledInt
+    {
+        int value;
+        readonly int ceil;
+
+        public CeiledInt(int value, int ceil)
+        {
+            Value = value;
+            this.ceil = ceil;
+        }
+
+        public int Value
+        {
+            get => Math.Min(value, ceil);
+            set => this.value = value;
+        }
+    }
+}
---
+++ Assets/AdvanceWars/Tests/Editor/DataStructures/CeiledIntTests.cs
@@ -0,0 +1,38 @@
+﻿using AdvanceWars.Runtime.Extensions.DataStructures;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace AdvanceWars.Tests.DataStructures
+{
+    public class CeiledIntTests
+    {
+        [Test]
+        public void InitialValueCanBeRetrieved()
+        {
+            new CeiledInt(value: 1, ceil: 2).Value.Should().Be(1);
+        }
+
+        [TestCase(1)]
+        [TestCase(2)]
+        public void ValueCanBeModified(int newValue)
+        {
+            var sut = new CeiledInt(value: 0, ceil: 2);
+            
+            sut.Value = newValue;
+
+            sut.Value.Should().Be(newValue);
+        }
+        
+        [Test]
+        public void ValueIsCeiled()
+        {
+            const int ceil = 2;
+            var sut = new CeiledInt(value: 1, ceil);
+
+            const int overflowIncrement = 2;
+            sut.Value += overflowIncrement;
+
+            sut.Value.Should().Be(ceil);
+        }
+    }
+}
