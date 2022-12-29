cb1e115f3307c9ae57ff355d7465f8021b9ba4c8
Xiphereal <lozanojalex@gmail.com>
2022-12-29 01:53:52 +0100

    refactor: change Forces to be of CeiledInt
    No estoy del todo contento con lo del .Value. La conversión implícita me ha dado dolores de cabeza, así que estoy 50/50 sobre si dejar estos cambios o no.

--- Assets/AdvanceWars/Tests/Editor/DataStructures/CeiledIntTests.cs
+++ Assets/AdvanceWars/Tests/Editor/DataStructures/CeiledIntTests.cs
@@ -17,12 +17,12 @@ namespace AdvanceWars.Tests.DataStructures
         public void ValueCanBeModified(int newValue)
         {
             var sut = new CeiledInt(value: 0, ceil: 2);
-            
+
             sut.Value = newValue;
 
             sut.Value.Should().Be(newValue);
         }
-        
+
         [Test]
         public void ValueIsCeiled()
         {
@@ -34,13 +34,35 @@ namespace AdvanceWars.Tests.DataStructures
 
             sut.Value.Should().Be(ceil);
         }
-        
+
         [Test]
         public void ValueCanBeImplicitlyConvertedToInt()
         {
             int sut = new CeiledInt(value: 1, ceil: 1);
-            
+
             sut.Should().Be(1);
+        }
+
+        [Test]
+        public void InitialValueCanNotOverflowCeil()
+        {
+            new CeiledInt(value: 2, ceil: 1).Value.Should().Be(1);
+        }
+
+        [Test]
+        public void CeiledInt_PlusInt_DoesNotOverflow()
+        {
+            var sut = new CeiledInt(value: 1, ceil: 1) + 1;
+
+            sut.Value.Should().Be(1);
+        }
+        
+        [Test]
+        public void Int_PlusCeiledInt_DoesNotOverflow()
+        {
+            var sut = 1 + new CeiledInt(value: 1, ceil: 1);
+
+            sut.Value.Should().Be(1);
         }
     }
 }
